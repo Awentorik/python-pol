227c5c3e9680b325f94fb21a611940ed07d10a17
Awentorik <147987763+Awentorik@users.noreply.github.com>
2023-10-15 16:46:04 +0300

    Add files via upload

---
+++ Лабораторная 1/3 Практика.py
@@ -0,0 +1,5 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+count_ = len(list_players)
+print(list_players[0:3])
+print(list_players[3:])
+# TODO Разделите участников на две команды
