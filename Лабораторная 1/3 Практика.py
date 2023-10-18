36d11c09986f89aeaf63ceb7e9c5187ebe3c9a47
Awentorik <147987763+Awentorik@users.noreply.github.com>
2023-10-18 11:00:44 +0300

    Update 3 Практика.py

--- Лабораторная 1/3 Практика.py
+++ Лабораторная 1/3 Практика.py
@@ -1,5 +1,5 @@
 list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
 count_ = len(list_players)
-print(list_players[0:3])
-print(list_players[3:])
-# TODO Разделите участников на две команды
+half_ = count_//2
+print(list_players[:half_])
+print(list_players[half_:])
